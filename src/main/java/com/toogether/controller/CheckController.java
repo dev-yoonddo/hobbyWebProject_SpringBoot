8077337f826f0fcc137099468fc38a204fd9be3f
yoonddo <oooo_0720@naver.com>
2024-02-08 23:56:06 +0900

    log4j2 사용 시작

---
+++ src/main/java/com/toogether/controller/CheckController.java
@@ -0,0 +1,13 @@
+package com.toogether.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class CheckController {
+
+}
